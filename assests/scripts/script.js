a1c71f8799286df7d1b91fb76d0a7d447f64dfca
Makai Post <postmakai@gmail.com>
2020-11-07 14:25:36 -0800

    changed js

---
+++ assests/scripts/script.js
@@ -0,0 +1,54 @@
+const nytKey = '2RxRwHylsMjejWvqW570N8yoeX9ZfHly'
+var getURL = (searchTerm, startYear='', endYear='') => {
+    let url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?' 
+        + `api-key=${nytKey}`
+        + `&q=${searchTerm}`
+        + (startYear ? `&begin_date=${startYear}0101` : '')
+        + (endYear ? `&end_date=${endYear}1231` : '')
+    return url
+}
+
+var termInput = $('#Term-input')
+var countInput = $('#record-Num-input')
+var startYearInput = $('#start-year')
+var endYearInput = $('#end-year')
+var searchButton = $('#searchBtn')
+var clearButton = $('#Clearbtn')
+var topArticlesDiv = $('#top-articles')
+
+function search() {
+    topArticlesDiv.html('')
+
+    var term = countInput.val()
+    var count = parseInt(countInput.val())
+    var start = parseInt(startYearInput.val())
+    var end = parseInt(endYearInput.val())
+
+    $.get(getURL(term, start, end))
+    .then(data => {
+        var data = data.response.docs
+    
+        for (let i = 0; i < count; i++) {
+            let doc = data[i];
+            console.log(doc);
+
+            let headline = $('<h2>').text(doc.headline.main)
+            let link = $('<a>').attr('href', doc.web_url).append(headline)
+
+            let dateArr = doc.pub_date.split('T')[0].split('-')
+            let dateStr = `${dateArr[1]}-${dateArr[2]}-${dateArr[0]}`
+            let date = $('<en>').text(dateStr)
+
+            let abstract = $('<p>').text(doc.abstract)
+
+            let article = $('<article>').append(link, date, abstract)
+
+            topArticlesDiv.append(article)
+        }
+    })
+}
+
+searchButton.on('click', event => {
+    event.preventDefault()
+    search()
+})
